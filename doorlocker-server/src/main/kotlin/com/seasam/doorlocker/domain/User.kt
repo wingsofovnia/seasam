1e94ff4ee9aa2f4eca3eba7016bd4b6d14efa661
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2018-08-30 09:02:30 +0200

    Add missed device & permission mutators/finds to User

--- doorlocker-server/src/main/kotlin/com/seasam/doorlocker/domain/User.kt
+++ doorlocker-server/src/main/kotlin/com/seasam/doorlocker/domain/User.kt
@@ -2,6 +2,7 @@ package com.seasam.doorlocker.domain
 
 import com.seasam.doorlocker.domain.credentials.UsernamePasswordCredentials
 import org.springframework.data.mongodb.core.mapping.Document
+import java.security.PublicKey
 import javax.validation.constraints.NotBlank
 
 @Document
@@ -26,7 +27,22 @@ class User(
 
     fun addDevice(device: Device) = devices.add(device)
 
+    fun findDevice(deviceKey: PublicKey) = devices.find { it.key == deviceKey }
+
+    fun removeDevice(deviceKey: PublicKey) = devices.removeIf { it.key == deviceKey }
+
+    fun removeDevice(device: Device) = devices.remove(device)
+
+
     fun addPermission(permission: Permission) = permissions.add(permission)
+
+    fun findPermission(thingId: ThingId) = permissions.find { it.thingId == thingId }
+
+    infix fun hasPermission(thingId: ThingId) = findPermission(thingId) != null
+
+    fun removePermission(thingId: ThingId) = permissions.removeIf { it.thingId == thingId }
+
+    fun removePermission(permission: Permission) = permissions.remove(permission)
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
