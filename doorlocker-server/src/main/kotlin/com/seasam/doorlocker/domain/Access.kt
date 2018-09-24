f19a9577d4f785f1687f93eba0451432d51d0d56
Elias Ovchynnikov <illia.ovchynnikov@gmail.com>
2018-09-24 11:06:52 +0200

    Extract Access to separate aggregate root (#2)

--- doorlocker-server/src/main/kotlin/com/seasam/doorlocker/domain/Access.kt
+++ doorlocker-server/src/main/kotlin/com/seasam/doorlocker/domain/Access.kt
@@ -1,11 +1,30 @@
 package com.seasam.doorlocker.domain
 
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 
-data class Access(
+@Document
+class Access(
+    val id: AccessId = AccessId(),
+
     val userId: UserId,
     val device: Device,
     val permission: Permission) {
 
     val timestamp: LocalDateTime = LocalDateTime.now()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Access
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
