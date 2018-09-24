8d623d89e813d89d2fd43b83a8be65f98970da0d
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2018-09-24 12:42:53 +0200

    Add Device Resource controller stub

---
+++ doorlocker-server/src/main/kotlin/com/seasam/doorlocker/application/api/DeviceResource.kt
@@ -0,0 +1,37 @@
+package com.seasam.doorlocker.application.api
+
+import com.seasam.doorlocker.application.api.dto.DeviceDto
+import com.seasam.doorlocker.domain.UserId
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
+
+@Validated
+@RestController
+@RequestMapping("/api/users")
+class DeviceResource {
+
+
+    @PostMapping("/{userId}/devices", produces = [MediaType.APPLICATION_JSON_VALUE],
+        consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun createUserDevice(@PathVariable userId: UserId, @RequestBody dto: DeviceDto) =
+        Mono.just(ResponseEntity<DeviceDto>(HttpStatus.NOT_IMPLEMENTED)) // Mono<ResponseEntity<DeviceDto>> HttpStatus.CREATED
+
+
+    @GetMapping("/{userId}/devices/{deviceKey}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getOneUserDevice(@PathVariable userId: UserId, @PathVariable dto: String) =
+        Mono.just(ResponseEntity<DeviceDto>(HttpStatus.NOT_IMPLEMENTED)) // <Mono<ResponseEntity<DeviceDto>> HttpStatus.OK
+
+
+    @GetMapping("/{userId}/devices", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getAllUserDevices(@PathVariable userId: UserId) =
+        Mono.just(ResponseEntity<DeviceDto>(HttpStatus.NOT_IMPLEMENTED)) // Flux<DeviceDto> HttpStatus.OK
+
+
+    @DeleteMapping("/{userId}/devices/{deviceKey}")
+    fun deleteUserDevice(@PathVariable userId: UserId, @PathVariable deviceKey: String) =
+        Mono.just(ResponseEntity<DeviceDto>(HttpStatus.NOT_IMPLEMENTED)) // Mono<ResponseEntity<Void>> HttpStatus.NO_CONTENT
+}
