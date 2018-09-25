385d343f296d88a3b539b5edf40ea0e3a0b3f3b2
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2018-09-25 06:30:02 +0200

    Implement Access Resource API

--- doorlocker-server/src/main/kotlin/com/seasam/doorlocker/application/api/AccessResource.kt
+++ doorlocker-server/src/main/kotlin/com/seasam/doorlocker/application/api/AccessResource.kt
@@ -1,26 +1,24 @@
 package com.seasam.doorlocker.application.api
 
-import com.seasam.doorlocker.application.api.dto.DeviceDto
+import com.seasam.doorlocker.application.api.dto.AccessDto
+import com.seasam.doorlocker.domain.AccessRepository
 import com.seasam.doorlocker.domain.ThingId
 import com.seasam.doorlocker.domain.UserId
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
 
 @Validated
 @RestController
 @RequestMapping("/api/accesses")
-class AccessResource {
+class AccessResource(val repository: AccessRepository) {
 
     @GetMapping("/accesses", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getAllAccesses(@RequestParam(required = false) thingId: ThingId,
                        @RequestParam(required = false) userId: UserId,
                        @RequestParam(required = false) period: String) =
-        Mono.just(ResponseEntity<DeviceDto>(HttpStatus.NOT_IMPLEMENTED)) // TODO: Flux<DeviceDto> HttpStatus.OK
+        repository.findAll().map { AccessDto.from(it) }
 }
