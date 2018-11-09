3f1c4c5488d53fc6a5a269ca27e3124d0af8291d
Stas <oalximiko@gmail.com>
2018-11-09 23:19:36 +0100

    at least it gives jwt token

--- doorlocker-server/src/main/kotlin/com/seasam/doorlocker/DoorLockerApplication.kt
+++ doorlocker-server/src/main/kotlin/com/seasam/doorlocker/DoorLockerApplication.kt
@@ -1,11 +1,29 @@
 package com.seasam.doorlocker
 
+import com.seasam.doorlocker.domain.User
+import com.seasam.doorlocker.domain.UserRepository
+import com.seasam.doorlocker.domain.credentials.password.Password
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.beans.factory.annotation.Autowired
+
+
 
 @SpringBootApplication
-class DoorLockerApplication
+class DoorLockerApplication(@Autowired
+                            private val repository: UserRepository) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        repository.deleteAll().block()
+        val email = "email1"
+        val password = Password.create("ololo")
+        val expectedUser = User(password = password, email = email, name = "Name")
+        repository.save(expectedUser).block()
+    }
 
+}
 fun main(args: Array<String>) {
     runApplication<DoorLockerApplication>(*args)
 }
+
+
